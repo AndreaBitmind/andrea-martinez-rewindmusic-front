0589d953d12258f9d2ca0c9ea84aef8cb01b68a6
Andrea Martinez <andrea.maort@gmail.com>
2022-09-03 11:53:14 +0200

    added user slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,7 +1,8 @@
 import { configureStore, ThunkAction, Action } from "@reduxjs/toolkit";
+import { userReducer } from "./store/features/users/slices/usersSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: { users: userReducer },
 });
 
 export type AppDispatch = typeof store.dispatch;
