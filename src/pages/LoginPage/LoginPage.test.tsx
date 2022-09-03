696a94a9fd07bd9a4fb9b3d000ee25b6ab185a3a
Andrea Martinez <andrea.maort@gmail.com>
2022-09-03 12:40:31 +0200

    added loginForm functionality ahd loginPage test

--- src/pages/LoginPage/LoginPage.test.tsx
+++ src/pages/LoginPage/LoginPage.test.tsx
@@ -1,14 +1,14 @@
 import { render, screen } from "@testing-library/react";
-import { BrowserRouter } from "react-router-dom";
+import Wrapper from "../../utils/Wrapper";
 import LoginPage from "./LoginPage";
 
 describe("Given the RegiterPage page", () => {
   describe("When it's instantiated", () => {
     test("Then should show 'Registro' in heading", () => {
       render(
-        <BrowserRouter>
+        <Wrapper>
           <LoginPage />
-        </BrowserRouter>
+        </Wrapper>
       );
 
       const expectedTitle = screen.getByText("Sign In");
