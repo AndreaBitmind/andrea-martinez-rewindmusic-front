82d15dbcc698363b262b6d79de3e7a93d350be33
Andrea Martinez <andrea.maort@gmail.com>
2022-09-04 19:47:46 +0200

    added card list component

---
+++ src/components/CardList/CardList.tsx
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import useApi from "../../hooks/useApi/useApi";
+import { RootState } from "../../store/store";
+
+const CardList = (): JSX.Element => {
+  const { getAllSongs } = useApi();
+  const songs = useSelector((state: RootState) => state.songs);
+
+  useEffect(() => {
+    getAllSongs();
+  }, [getAllSongs]);
+
+  return (
+    <ul className="songs-list">
+      {songs.map((song) => (
+        <li className="songs-list__item" key={song.id}>
+          {song.album}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default CardList;
