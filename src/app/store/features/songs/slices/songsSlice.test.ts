b5c1bd46bf6288984e63f78586ee49411614c570
Andrea Martinez <andrea.maort@gmail.com>
2022-09-04 12:25:55 +0200

    added songs slice test

---
+++ src/app/store/features/songs/slices/songsSlice.test.ts
@@ -0,0 +1,38 @@
+import { Songs } from "../../../../../interfaces/users/Songs";
+import songsSlice, { loadAllSongsActionCreator } from "./songsSlice";
+
+describe("Given a songs slice", () => {
+  describe("When invoked with an initial state as previous songs and a loadAllSongs with a fake list of songs", () => {
+    test("Then it should return a list with two songs", () => {
+      const initialState: Songs = [];
+
+      const fakeListSongs: Songs = [
+        {
+          songName: "We are your friends",
+          album: "We are your friends",
+          year: "2001",
+          band: "Justice, Simian",
+          instrument: ["guitar"],
+          image: "http://picture.com",
+          embeded: "prueba2",
+        },
+        {
+          songName: "Barbie girl",
+          album: "vicios y virtudes",
+          year: "2001",
+          band: "SFDK",
+          instrument: ["piano"],
+          image: "http://picture.com",
+          embeded: "prueba2",
+        },
+      ];
+
+      const songs = songsSlice(
+        initialState,
+        loadAllSongsActionCreator(fakeListSongs)
+      );
+
+      expect(songs).toStrictEqual(fakeListSongs);
+    });
+  });
+});
