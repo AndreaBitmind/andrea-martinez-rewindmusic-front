0cbd02a6d49dbfc66844ca688dd272a95ab0637e
Andrea Martinez <andrea.maort@gmail.com>
2022-09-07 20:16:26 +0200

    added songDetails test

---
+++ src/components/SongDetails/SongDetails.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from "@testing-library/react";
+import { Provider } from "react-redux";
+import { BrowserRouter } from "react-router-dom";
+import SongDetailsPage from "../../pages/SongDetailsPage/SongDetailsPage";
+import { store } from "../../store/store";
+
+describe("Given a SongDetails component", () => {
+  describe("When instantiated", () => {
+    test("Then it should display a title, an image and a list of details", () => {
+      render(
+        <Provider store={store}>
+          <BrowserRouter>
+            <SongDetailsPage />
+          </BrowserRouter>
+        </Provider>
+      );
+
+      const elements = [
+        screen.getByText("SONG DETAILS"),
+        screen.getByAltText("albumPhoto"),
+        screen.getByRole("list"),
+      ];
+
+      elements.forEach((element) => expect(element).toBeInTheDocument());
+    });
+  });
+});
