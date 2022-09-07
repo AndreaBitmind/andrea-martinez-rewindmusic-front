539aa2f7266e9e7b3e26a8ae3182568c7f36121e
Andrea Martinez <andrea.maort@gmail.com>
2022-09-07 10:44:25 +0200

    added deleteSong function in custom hook

--- src/hooks/useApi/useApi.ts
+++ src/hooks/useApi/useApi.ts
@@ -1,9 +1,13 @@
 import axios from "axios";
 import { useCallback } from "react";
 import { toast } from "react-toastify";
-import { loadAllSongsActionCreator } from "../../store/features/songs/slices/songsSlice";
+import {
+  deleteSongActionCreator,
+  loadAllSongsActionCreator,
+} from "../../store/features/songs/slices/songsSlice";
 
 import { useAppDispatch } from "../../store/hooks";
+import { successModal } from "../useUser/useUser";
 
 const apiURL = process.env.REACT_APP_API_URL;
 
@@ -36,9 +40,32 @@ const useApi = () => {
       errorModal("Oops, something went wrong :(");
     }
   }, [dispatch]);
+
+  const deleteSong = useCallback(
+    async (songId: string) => {
+      const token = localStorage.getItem("token");
+      const deleteSongsUrl = `${apiURL}songs/`;
+
+      try {
+        await axios.delete(`${deleteSongsUrl}${songId}`, {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
+
+        dispatch(deleteSongActionCreator(songId));
+      } catch (error) {
+        errorModal("Oops, something went wrong :(");
+      }
+      successModal("Great! The wish has been deleted!");
+    },
+    [dispatch]
+  );
+
   toast.dismiss();
   return {
     getAllSongs,
+    deleteSong,
   };
 };
 
