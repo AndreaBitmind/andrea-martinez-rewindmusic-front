2837aeedd74378639cc3a7243ddfb99280ccac6e
Andrea Martinez <andrea.maort@gmail.com>
2022-09-08 17:30:59 +0200

    added songDetails component

--- src/components/SongDetails/SongDetails.tsx
+++ src/components/SongDetails/SongDetails.tsx
@@ -1,18 +1,36 @@
-export const SongDetails = (): JSX.Element => {
+import { NavLink } from "react-router-dom";
+
+interface SongDetailsProps {
+  songName: string;
+  album: string;
+  year: string;
+  band: string;
+  image: string;
+  instrument: string[];
+}
+
+export const SongDetails = ({
+  songName,
+  album,
+  year,
+  band,
+  image,
+  instrument,
+}: SongDetailsProps): JSX.Element => {
   return (
     <>
+      <NavLink className="navlink-more" to="/songs">
+        Back
+      </NavLink>
       <div className="song-detail__title">SONG DETAILS</div>
       <section className="song-detail__details">
-        <img
-          src="https://m.media-amazon.com/images/I/81NYIh0POrL._SS500_.jpg"
-          alt="albumPhoto"
-        />
+        <img src={image} alt={`${band} album cover`} />
         <ul className="song-detail__details-list">
-          <li className="song-detail">Song:</li>
-          <li className="song-detail">Album:</li>
-          <li className="song-detail">Year:</li>
-          <li className="song-detail">Band:</li>
-          <li className="song-detail">Instrument:</li>
+          <li className="song-detail">{`Song: ${songName}`}</li>
+          <li className="song-detail">{`Album: ${album}`}</li>
+          <li className="song-detail">{`Year: ${year}`}</li>
+          <li className="song-detail">{`Band: ${band}`}</li>
+          <li className="song-detail">{`Instrument: ${instrument[0]}, ${instrument[1]}, ${instrument[2]}`}</li>
         </ul>
       </section>
     </>
