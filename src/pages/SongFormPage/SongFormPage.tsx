4bb738f01f4443febe8ee14b7b693f6a22d0789b
Andrea Martinez <andrea.maort@gmail.com>
2022-09-11 12:56:37 +0200

    added songFormPage with props

--- src/pages/SongFormPage/SongFormPage.tsx
+++ src/pages/SongFormPage/SongFormPage.tsx
@@ -1,7 +1,26 @@
+import { useParams } from "react-router-dom";
 import SongForm from "../../components/SongForm/SongForm";
+import { IcreateSong, ImodifySong } from "../../interfaces/users/Songs";
+import { useAppSelector } from "../../store/hooks";
 
 const SongFormPage = (): JSX.Element => {
-  return <SongForm />;
+  const songs = useAppSelector((state) => state.songs);
+  const { id } = useParams();
+
+  const songModifyOrCreate: ImodifySong =
+    songs && id
+      ? (songs.find((song) => id === song.id) as IcreateSong)
+      : {
+          songName: "",
+          album: "",
+          year: "",
+          band: "",
+          image: "",
+          firstInstrument: "",
+          secondInstrument: "",
+        };
+
+  return <SongForm initialState={songModifyOrCreate} />;
 };
 
 export default SongFormPage;
